8307bc334dd57a88ea9828abde7836d28bc28308
HackYourFuture Belgium <manjuparbha@gmail.com>
2019-03-11 23:25:33 +0100

    manju create_user_profile_br

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
-import Body from "../components/Body";
+//import Body from "../components/Body";
 import Footer from "../components/Footer";
+import UserProfile from "../components/UserProfile";
 
 const Home = props => {
   return (
     <div>
       <Header />
-      <Body />
+      {/* <Body /> */}
+      <UserProfile />
       <Footer />
     </div>
   );
