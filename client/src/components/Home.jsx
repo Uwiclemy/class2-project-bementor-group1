93e8121ad8c3c5c7b560a6e5f9dec6bcd8c2f465
HackYourFuture Belgium <manjuparbha@gmail.com>
2019-03-12 00:54:42 +0100

    reverting my wrong file  in master

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
-//import Body from "../components/Body";
+import Body from "../components/Body";
 import Footer from "../components/Footer";
-import UserProfile from "../components/UserProfile";
 
 const Home = props => {
   return (
     <div>
       <Header />
-      {/* <Body /> */}
-      <UserProfile />
+      <Body />
       <Footer />
     </div>
   );
