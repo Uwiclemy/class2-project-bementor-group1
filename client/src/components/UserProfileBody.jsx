ec480957bbbd41bb99d14c88465e607dd94f276f
Joel <camacho-joel@hotmail.com>
2019-03-12 23:43:20 +0100

    Improvements to the User Profile's css

--- client/src/components/UserProfileBody.jsx
+++ client/src/components/UserProfileBody.jsx
@@ -21,24 +21,49 @@ const listSkills = skills.map(skill => <li>{skill}</li>);
 class UserProfileBody extends Component {
   render() {
     return (
-      <div>
-        <div className="user-intro">
+      <div className="user-wrapper">
+        <div className="user">
           <img className="user-image" />
-          <h2 className="user-name">Michiel Leyman</h2>
-          <b />
-          <h2 className="user-quote">I make useful things for other humans</h2>
+          <div className="user-intro">
+            <h2 className="user-name">Michiel Leyman</h2>
+
+            <h2 className="user-quote">
+              I make useful things for other humans
+            </h2>
+          </div>
         </div>
-        <h3 className="user-title1">Campus</h3>
-        <h3 className="user-title2">Becentral</h3>
-        <h3 className="user-title2">Bio</h3>
+        <div className="user-title">
+          <h3 className="user-title1">Campus</h3>
+          <h3 className="user-title2">Becentral</h3>
+        </div>
+        <h3 className="user-title3">Bio</h3>
         <p className="user-description">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          inventore incidunt impedit, atque debitis nostrum consectetur,
+          dolorum, doloremque repellat cupiditate numquam enim odio iste ratione
+          officia? Veritatis velit facere necessitatibus! Lorem, ipsum dolor sit
+          amet consectetur adipisicing elit. Voluptas inventore incidunt
+          impedit, atque debitis nostrum consectetur, dolorum, doloremque
+          repellat cupiditate numquam enim odio iste ratione officia? Veritatis
+          velit facere necessitatibus! Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptas inventore incidunt impedit, atque debitis
+          nostrum consectetur, dolorum, doloremque repellat cupiditate numquam
+          enim odio iste ratione officia? Veritatis velit facere necessitatibus!
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
           inventore incidunt impedit, atque debitis nostrum consectetur,
           dolorum, doloremque repellat cupiditate numquam enim odio iste ratione
           officia? Veritatis velit facere necessitatibus!
         </p>
-        <ul className="user-interests">{listInterest}</ul>
-        <ul className="user-skill">{listSkills}</ul>
+        <div className="user-info">
+          <div className="user-interests-wraper">
+            <h3 className="user-interests">Interests</h3>
+            <ul className="user-interests-list">{listInterest}</ul>
+          </div>
+          <div className="user-skills-wraper">
+            <h3 className="user-skills">Skills</h3>
+            <ul className="user-skills-list">{listSkills}</ul>
+          </div>
+        </div>
       </div>
     );
   }
