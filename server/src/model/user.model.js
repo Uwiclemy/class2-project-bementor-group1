10f69b54d015f2372128762600bf388c92db5bc4
UWINGENEYE Clementine <uwiclemy@gmail.com>
2019-03-12 22:16:45 +0100

    model

---
+++ server/src/model/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    img: {
+      data: Buffer,
+      contentType: String
+    },
+    tagLine: {
+      type: String,
+      required: true
+    },
+    campus: {
+      type: String,
+      required: true
+    },
+    bio: {
+      type: String,
+      required: true
+    },
+    interests: {
+      type: [String],
+      required: true
+    },
+    skills: {
+      type: [string],
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
