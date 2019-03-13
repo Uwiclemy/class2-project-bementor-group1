e20886ab52a94c6ce94e07244e1cb8556a72b549
HackYourFuture Belgium <manjuparbha@gmail.com>
2019-03-14 00:59:52 +0100

    code updated for dynamic user data

---
+++ client/src/components/MocUser.js
@@ -0,0 +1,31 @@
+// Fake contact data for testing purposes.
+
+const MocUser = [
+  {
+    id: 1,
+    firstName: "aaaa",
+    lastName: "AAAA",
+    tagLine: "I make aaaa",
+    campus: "BeCentral",
+    Bio:
+      "I make aaaa.....something....something Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    profileImage:
+      "https://enigmeta.s3.amazonaws.com/hackyourfuture/avatars/male-1.jpg",
+
+    interests: [
+      "First Interest",
+      "Second Interest",
+      "Third Interest",
+      "Forth Interest",
+      "Fifth Interest"
+    ],
+    skills: [
+      "First skill",
+      "Second skill",
+      "Third skill",
+      "Forth skill",
+      "Fifth skill"
+    ]
+  }
+];
+export default MocUser;
--- client/src/components/UserProfileBody.jsx
+++ client/src/components/UserProfileBody.jsx
@@ -1,63 +1,41 @@
 import React, { Component } from "react";
+import MocUser from "./MocUser";
 
-const interests = [
-  "First Interest",
-  "Second Interest",
-  "Third Interest",
-  "Forth Interest",
-  "Fifth Interest"
-];
-const listInterest = interests.map(interest => (
-  <li>
-    <a className="list">{interest}</a>
-  </li>
-));
+class UserProfilePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: MocUser
+    };
+  }
+  renderUser(userprofile) {
+    const listInterest = userprofile.interests.map(interest => (
+      <li>{interest}</li>
+    ));
 
-const skills = [
-  "First skill",
-  "Second skill",
-  "Third skill",
-  "Forth skill",
-  "Fifth skill"
-];
-const listSkills = skills.map(skill => <li>{skill}</li>);
-
-class UserProfileBody extends Component {
-  render() {
+    const listSkills = userprofile.skills.map(skills => <li>{skills}</li>);
     return (
       <div className="user-wrapper">
         <div className="user">
-          <img className="user-image" />
+          <img
+            className="user-image"
+            src={userprofile.profileImage}
+            alt="userimage"
+          />
           <div className="user-intro">
-            <h2 className="user-name">Michiel Leyman</h2>
-
-            <h2 className="user-quote">
-              I make useful things for other humans
+            <h2 className="user-name">
+              {userprofile.firstName} {userprofile.lastName}
             </h2>
+
+            <h2 className="user-quote">{userprofile.tagLine}</h2>
           </div>
         </div>
         <div className="user-title">
           <h3 className="user-title1">Campus</h3>
-          <h3 className="user-title2">Becentral</h3>
+          <h3 className="user-title2">{userprofile.campus}</h3>
         </div>
         <h3 className="user-title3">Bio</h3>
-        <p className="user-description">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          inventore incidunt impedit, atque debitis nostrum consectetur,
-          dolorum, doloremque repellat cupiditate numquam enim odio iste ratione
-          officia? Veritatis velit facere necessitatibus! Lorem, ipsum dolor sit
-          amet consectetur adipisicing elit. Voluptas inventore incidunt
-          impedit, atque debitis nostrum consectetur, dolorum, doloremque
-          repellat cupiditate numquam enim odio iste ratione officia? Veritatis
-          velit facere necessitatibus! Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Voluptas inventore incidunt impedit, atque debitis
-          nostrum consectetur, dolorum, doloremque repellat cupiditate numquam
-          enim odio iste ratione officia? Veritatis velit facere necessitatibus!
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          inventore incidunt impedit, atque debitis nostrum consectetur,
-          dolorum, doloremque repellat cupiditate numquam enim odio iste ratione
-          officia? Veritatis velit facere necessitatibus!
-        </p>
+        <p className="user-description">{userprofile.Bio}</p>
         <div className="user-info">
           <div className="user-interests-wraper">
             <h3 className="user-interests">Interests</h3>
@@ -71,5 +49,13 @@ class UserProfileBody extends Component {
       </div>
     );
   }
+  render() {
+    const { users } = this.state;
+
+    const $userProfile = users.map(userprofile => this.renderUser(userprofile));
+
+    return <div>{$userProfile}</div>;
+  }
 }
-export default UserProfileBody;
+
+export default UserProfilePage;
