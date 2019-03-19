6ebb1310106ea55e2bb0e0673dbef154bd5be10d
Joel <camacho-joel@hotmail.com>
2019-03-19 21:01:39 +0100

    creation index on mentor model

--- server/src/model/mentor.model.js
+++ server/src/model/mentor.model.js
@@ -1,27 +1,40 @@
 const mongoose = require("mongoose");
 
-const MentorSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  img: {
-    type: String
-    //contentType: String
-  },
+const mentorSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    img: {
+      type: String
+      //contentType: String
+    },
 
-  shortBio: {
-    type: String,
-    required: true
+    shortBio: {
+      type: String,
+      required: true
+    },
+    interests: {
+      type: [String],
+      required: true
+    }
   },
-  interests: {
-    type: [String],
-    required: true
+  {
+    timestamps: true
   }
+);
+
+mentorSchema.index({
+  firstName: "text",
+  lastName: "text",
+  img: "image",
+  shortBio: "text",
+  interests: "text"
 });
 
-module.exports = mongoose.model("Mentor", MentorSchema);
+module.exports = mongoose.model("Mentor", mentorSchema);
