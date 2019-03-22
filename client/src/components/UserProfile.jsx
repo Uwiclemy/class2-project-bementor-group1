f161d83881d2a202eb18d6b033793ef9264732e5
JoeCamacho <42816478+JoeCamacho@users.noreply.github.com>
2019-03-22 18:59:05 +0100

    Update UserProfile.jsx
    Changed the code to import the SecondaryHeader component from right file

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
-import Header from "./MainHeader";
-
+import SecondaryHeader from "./SecondaryHeader";
 import Footer from "../components/Footer";
 import UserProfileBody from "../components/UserProfileBody";
 
