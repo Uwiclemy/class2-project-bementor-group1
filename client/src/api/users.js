9236aaeca687099d04092d13f1664528c6fb7dfe
Joel <camacho-joel@hotmail.com>
2019-03-25 22:07:03 +0100

    fixing routes in user.js

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,11 +1,11 @@
 const API_URL = "http://localhost:4000";
 
 export const getUsers = () => {
-  return fetch(`${API_URL}/user`).then(response => response.json());
+  return fetch(`${API_URL}/api/user`).then(response => response.json());
 };
 
 export const searchUsers = query => {
-  return fetch(`${API_URL}/user/search?text=${query}`).then(response =>
+  return fetch(`${API_URL}/api/user/search?text=${query}`).then(response =>
     response.json()
   );
 };
