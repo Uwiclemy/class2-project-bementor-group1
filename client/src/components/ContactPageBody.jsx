e1ed4de839583ac52036757af35d05165a9b5930
HackYourFuture Belgium <manjuparbha@gmail.com>
2019-03-25 21:22:44 +0100

    created contact page and implemented linked with nav bar element

--- client/src/components/ContactPageBody.jsx
+++ client/src/components/ContactPageBody.jsx
@@ -8,23 +8,21 @@ const ContactPageBody = props => {
       <div className="contact-info-address">
         <i className="fas fa-map-marker" />
         <span>
-          <p>adresss1</p>
+          <p>adresss aaaaa adresss aaaaa </p>
+          <p>adresss aaaaa adresss aaaaa adresss aaaa</p>
         </span>
       </div>
 
       <div className="contact-info-email">
         <i className="fas fa-envelope-square" />
         <a href="mailto:info@bementor.be">
-          {/* <a href="mailto:info@bementor.be" class="footer-infos"> */}
           <span>info@bementor.be</span>
         </a>
       </div>
 
       <div className="contact-infos-tel">
         <i className="fas fa-phone" />
-
-        {/* <i class="fa fa-phone" aria-hidden="true" /> */}
-        <a href="tel:+32 (0)472 37 78 92;">+32 (0)472 37 78 92</a>
+        <a href="tel:+32 (0)xxx xx xx xx;">+32 (0)xxx xx xx xx</a>
       </div>
     </div>
   );
